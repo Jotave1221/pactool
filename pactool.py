e217c0c77b45f32035cfdbcf8db971f1c7e6c8fe
Jotave1221 <joaovitorkrugerdias@gmail.com>
2026-09-14 22:18:03 -0300

    base,basic,idk

---
+++ pactool.py
@@ -0,0 +1,31 @@
+import shutil
+import subprocess
+
+update = {
+    "apt": [["sudo", "apt", "update"], ["sudo", "apt", "full-upgrade", "-y"]],
+    "pacman": [["sudo", "pacman", "-Syu", "--noconfirm"]],
+    "dnf": [["sudo", "dnf", "upgrade", "--refresh", "-y"]],
+    "flatpak": [["flatpak", "update", "-y"]]
+}
+
+clean = {
+    "apt": [["sudo", "apt", "autoremove", "-y"], ["sudo", "apt", "autoclean"]],
+    "pacman": [["sudo", "pacman", "-Sc", "--noconfirm"]],
+    "dnf": [["sudo", "dnf", "autoremove", "-y"], ["sudo", "dnf", "clean", "all"]],
+    "flatpak": [["flatpak", "uninstall", "--unused", "-y"]]
+}
+
+update=input("would you like to update now?[Y/n]").lower()
+
+def execute():
+    if update==Y:
+        for tool in update:
+            if shutil.which(tool):
+                for cmds in toll:
+                    subprocess.run(cmds)
+    for tool in clean:
+        if shutil.which(tool):
+            for cmds in toll:
+                subprocess.run(cmds)
+    
+execute()
